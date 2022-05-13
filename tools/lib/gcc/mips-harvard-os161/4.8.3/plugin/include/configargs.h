52f95863aabeefbcaad0da82ead16ed80c008660
Simone Zanella <simone060699@gmail.com>
2022-05-13 10:50:42 +0000

    Clean OS161 version

---
+++ tools/lib/gcc/mips-harvard-os161/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/os161/gcc-4.8.3+os161-2.1/configure --enable-languages=c,lto --nfp --disable-shared --disable-threads --disable-libmudflap --disable-libssp --disable-libstdcxx --disable-nls --target=mips-harvard-os161 --prefix=/usr/local/os161/";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
